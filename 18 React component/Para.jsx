7bd067d4fab551b50172221b2269f1cff6a8e4d3
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-22 01:21:27 +0530

    paragraph component

---
+++ 18 React component/Para.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function List(){
+    return(
+        <ul>
+            <li>THIS IS</li>
+            <li>Comming from</li>
+            <li>List component</li>
+        </ul>
+    );
+}
+export default List;
