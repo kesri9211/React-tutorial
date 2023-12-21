974f2f486c33368310b6a3530e2a3c948c85945f
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-22 01:20:52 +0530

    Create Heading.jsx

---
+++ 18 React component/Heading.jsx
@@ -0,0 +1,12 @@
+//creating React component
+/*
+1- First letter of file name should be capital with extension .jsx
+2- function name should be same as file name
+*/
+import React from "react";
+//creating function
+function Heading(){
+    return <h1>Hello my name is kesri nandan shukla</h1>
+}
+//then export also
+export default Heading;
