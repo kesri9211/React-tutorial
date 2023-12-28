e0db409bc0a22ca72e91d8d89d45f94e67175025
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-28 19:45:05 +0530

    component sData - array of object (store data)

---
+++ 22 props, arrayofObject/sData.jsx
@@ -0,0 +1,28 @@
+//array of objects
+const sData=[
+    {
+        imgsrc:"https://f4.bcbits.com/img/a2164492178_10.jpg",
+        title:"DARK",
+        catagory:"A Netflix Orignal Series",
+        sLink:"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        imgsrc:"https://assets.gadgets360cdn.com/pricee/assets/product/202308/Life_on_Our_Planet_poster_1692868263.jpg",
+        title:"Life on our Planet", 
+        catagory:"A Netflix Orignal Series",
+        sLink:"https://www.netflix.com/in/title/80213846",
+    },
+    {
+        imgsrc:"https://eldiariony.com/wp-content/uploads/sites/2/2018/04/stranger-things.jpg?quality=80&strip=all&w=1200",
+        title:"Stranger Things", 
+        catagory:"A Netflix Orignal Series" ,
+        sLink:"https://www.netflix.com/in/title/80057281",
+    },
+    {
+        imgsrc:"https://static1.moviewebimages.com/wordpress/wp-content/uploads/2023/08/rebel-moon-part-one-a-child-of-fire-poster.jpg",
+        title:"Rebel Moon—Part One: A Child of Fire", 
+        catagory:"A Netflix Orignal Series" ,
+        sLink:"https://www.netflix.com/in/title/81464239",
+    }
+];
+export default sData;
