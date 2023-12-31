7268e73d5afaae2da6983e34f79bb878b4f09268
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-31 16:40:44 +0530

    Create index.js

---
+++ 26 if-else React/index.js
@@ -0,0 +1,10 @@
+//#26 If Else Statement in React JS | Conditional Rendering in ReactJS
+/*
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App/>,document.getElementById('root'));
