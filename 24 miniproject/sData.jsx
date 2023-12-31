3e93452ec2eb2adc49c0b715f62674050d56cd32
Kesri N. Shukla <138159327+kesri9211@users.noreply.github.com>
2023-12-31 13:03:17 +0530

    Create sData.jsx

---
+++ 24 miniproject/sData.jsx
@@ -0,0 +1,46 @@
+//array of objects
+const sData=[
+    {
+        id:1,
+        imgsrc:"https://f4.bcbits.com/img/a2164492178_10.jpg",
+        title:"DARK",
+        catagory:"A Netflix Orignal Series",
+        sLink:"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        id:2,
+        imgsrc:"https://assets.gadgets360cdn.com/pricee/assets/product/202308/Life_on_Our_Planet_poster_1692868263.jpg",
+        title:"Life on our Planet", 
+        catagory:"A Netflix Orignal Series",
+        sLink:"https://www.netflix.com/in/title/80213846",
+    },
+    {
+        id:3,
+        imgsrc:"https://eldiariony.com/wp-content/uploads/sites/2/2018/04/stranger-things.jpg?quality=80&strip=all&w=1200",
+        title:"Stranger Things", 
+        catagory:"A Netflix Orignal Series" ,
+        sLink:"https://www.netflix.com/in/title/80057281",
+    },
+    {
+        id:4,
+        imgsrc:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIF.AjlCBUL7SEMMAzqE0KEsag%26pid%3DApi&f=1&ipt=673493ca7ec2f3ef89333a6b383f66ae40c8152f7190e217876cfee3d665c7e4&ipo=images",
+        title:"Rebel Moon—Part One", 
+        catagory:"A Netflix Orignal Series" ,
+        sLink:"https://www.netflix.com/in/title/81464239",
+    },
+    {
+        id:5,
+        imgsrc:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.rETpfncggFiqXfmeMgn4vwHaHa%26pid%3DApi&f=1&ipt=2381ed7e034277046bff63d7defcc5d9aa044d2d614876d9d27699d11fba6e8e&ipo=images",
+        title:"Leave the world behind", 
+        catagory:"A Netflix Orignal Series" ,
+        sLink:"https://www.netflix.com/in/title/81314956",
+    },
+    {
+        id:6,
+        imgsrc:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.C6KzF5gZSR9bbUbnJooopgHaLH%26pid%3DApi&f=1&ipt=52cf93aeaf7606ce3a07645f73fcc16686e70351f8e5455c9f96040642ffe082&ipo=images",
+        title:"Chicken Run: Dawn of the Nugget", 
+        catagory:"A Netflix Orignal Series" ,
+        sLink:"https://www.netflix.com/in/title/81223025"
+    }
+];
+export default sData;
